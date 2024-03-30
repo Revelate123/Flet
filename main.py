d49d51dc50695f59211bd298735dd647e93a0ad3
Revelate123 <thomasduffett4@gmail.com>
2024-03-31 07:48:38 +1100

    31/03/2024
    - Major changes

--- main.py
+++ main.py
@@ -1,41 +1,159 @@
-import flask
-from flask_restful import Resource, Api, request
-import concrete_column
+from flask import Flask, flash, redirect, render_template, request, session, jsonify
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import datetime
+import sqlite3
+from helpers import apology, login_required
 
-app = flask.Flask(__name__)
-api = Api(app)
+con = sqlite3.connect("users.db")
+db = con.cursor()
+
+app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 
 
 @app.route('/', methods=['GET', 'POST'])
+@login_required
+def main():
+    #TODO: create login page
+
+    return render_template("login.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == 'POST':
+    """Log user in"""
 
-        x = {}
-        x.update(request.json)
+    # Forget any user_id
+    session.clear()
 
-        x = concrete(x)
-        print(x)
-        print(request.json)
-        print('test2')
-        return x
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
     else:
-        x = {'sectionType': 'Circular', 'Diameter': '200', 'Breadth': '1000', 'Depth': '15', 'Reinforcement': '8N20',
-             'output': ['Test'], 'fc':'32','fsy':'500','cover':'30'}
-        print('test1')
-        return x
-def concrete(x):
-    try:
-        if x['sectionType'] == 'Circular':
-            print('hello')
-            C = concrete_column.Column(x['sectionType'],int(x['Diameter']),0,0,x['Reinforcement'],int(x['fc']),int(x['fsy']),int(x['cover']))
-            print('success')
-            x['output'] = C.values
-            print(C.Squash_point())
-            print(C.Ag)
-    except:
-        x['output'] = 'Provide values'
-        print('yes')
-    return x
+        return render_template("login.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 400)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 400)
+
+        elif not request.form.get("confirmation"):
+            return apology("must provide password", 400)
+
+        elif request.form.get("password") != request.form.get("confirmation"):
+            return apology("passwords do not match", 400)
+
+        username = request.form.get("username")
+        password = generate_password_hash(request.form.get("password"))
+        # Query database for username
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", username
+        )
+
+        # Ensure username exists and password is correct
+        if len(rows) != 0:
+            return apology("Username already exists", 400)
+
+        db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", username, password)
+
+        # Redirect user to home page
+        return redirect("/login")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+@app.route("/change_password", methods=["GET", "POST"])
+@login_required
+def change_password():
+    """Change a users password"""
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # Ensure password was submitted
+        if not request.form.get("old_password") or not request.form.get("new_password") or not request.form.get("confirmation"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute(
+            "SELECT * FROM users WHERE id = ?", session["user_id"]
+        )
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("old_password")
+        ):
+            return apology("incorrect password", 403)
+
+        elif request.form.get("new_password") != request.form.get("confirmation"):
+            return apology("passwords do not match", 403)
+
+        password = generate_password_hash(request.form.get("new_password"))
+
+        db.execute("UPDATE users SET username = ?, hash = ? WHERE id = ?", rows[0]['username'], password, session['user_id'])
+
+        # Redirect user to home page
+        return redirect("/login")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("change_password.html")
+    # return history
+    return render_template("change_password.html")
 
 
 if __name__ == '__main__':
